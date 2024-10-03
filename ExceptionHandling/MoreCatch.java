ec873b3701f107c355efc22ad72f8b69b3f0e32f
Yashvanth <148965124+Yashvanthk05@users.noreply.github.com>
2024-10-03 12:55:27 +0530

    Update MoreCatch.java

--- ExceptionHandling/MoreCatch.java
+++ ExceptionHandling/MoreCatch.java
@@ -2,7 +2,7 @@ public class MoreCatch{
     public static void main(String[] args){
         try{
             int a[]=new int[5];
-            a[5]=30/0;
+            a[5]=30/0;//two exceptions are present ArrayIndexOutOfBoundsException and ArithmeticException
         }catch(ArithmeticException e){
             System.out.println("Arithmetic Exception Occurs");
         }catch(ArrayIndexOutOfBoundsException e){
